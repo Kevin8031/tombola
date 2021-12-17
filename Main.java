1cb23802c4262e5ac4675bc52eb2fda09e572a5c
Kevin8031 <concilio.kevin@gmail.com>
2021-12-17 16:46:51 +0100

    Aggiunta scelta

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        new Gioco(Gioco.giocatore.giocatore);
+        Scanner scanner = new Scanner(System.in);
+
+        if(scanner.nextInt() == 1)
+            new Gioco(Gioco.playerType.giocatore);
+        else
+            new Gioco(Gioco.playerType.tabellone);
     }
 }
