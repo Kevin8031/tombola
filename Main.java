3de6a158f6df9245b643a033e1680d2adf1ef28b
Kevin8031 <concilio.kevin@gmail.com>
2021-12-19 22:48:32 +0100

    ?

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.net.NetworkInterface;
+import java.net.SocketException;
+
 public class Main {
     public static void main(String[] args) {
+        // try {
+        //     for(int i = 0; i < 1000; i++)
+        //         System.out.println(NetworkInterface.getByIndex(i)); 
+        // } catch (SocketException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
         new Gioco();
     }
 }
