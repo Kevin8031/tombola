fb42b967e7cf5c07dccbe1fa90e3689f76d7084c
Kevin8031 <concilio.kevin@gmail.com>
2021-12-20 09:41:28 +0100

    Funziona

--- Giocatore.java
+++ Giocatore.java
@@ -9,134 +9,39 @@ import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.MembershipKey;
 import java.util.Scanner;
-import javax.swing.*;
-import java.awt.*;
-import java.net.*;
-import java.io.IOException;
 
 public class Giocatore extends Tabella {
-    // attributes
-    private JFrame frame;
-    private JPanel panel1;
-    private JPanel panel2;
-    private JLabel numero;
-    private Tabella tabella;
-    private GridLayout griglia;
-    private Socket socket;
-    private JButton[] caselle;
+	private JFrame frame;
+	private JPanel panel1;
+	private JPanel panel2;
+	private GridLayout griglia;
+	private JButton[] caselle;
+	private JLabel labelNumero;
+	private JPanel statusPanel;
 
-    // constructor
-    Giocatore(JFrame parent) {
-        frame = new JFrame("Tombola");
-        panel1 = new JPanel();
-        panel2 = new JPanel();
-        numero = new JLabel("-Numero-");
-        tabella = new Tabella();
-        griglia = new GridLayout(3, 9, 3, 3);
-        socket = new Socket();
-        caselle = new JButton[90];
-
-        frame.setSize(600, 350);
-        frame.setLayout(new GridLayout(2,1));
-        
-        numero.setVerticalAlignment(JLabel.BOTTOM);
-        numero.setHorizontalTextPosition(JLabel.CENTER);
-        numero.setVerticalTextPosition(JLabel.CENTER);
-        numero.setForeground(Color.WHITE);
-        numero.setFont(new Font("Roboto", Font.BOLD, 36));
-
-        panel1.setLayout(new GridBagLayout());
-        panel1.setBackground(new Color(74, 0, 255));
-        panel1.add(numero);
+	private int numero;
 
 	private boolean[] combo;
 
-        tabella.generaTabella();
-        
-        GeneraTabella();
-        
-        frame.add(panel1);
-        frame.add(panel2);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private Socket socket;
+	private Scanner inputStream;
+	private PrintStream outputStream;
+	private MulticastSocket multicastSocket;
+	private Thread rThread;
+	private Thread multicastThread;
+	private byte[] buf;
 
-        frame.setJMenuBar(new JMenuBar() {
-            {
-                
-                this.setBackground(new Color(16, 7, 232));
-                this.setForeground(Color.WHITE);
-                add(new JMenu("File") {
-                    {
-                        this.setBackground(new Color(16, 7, 232));
-                        this.setForeground(Color.WHITE);
-                        add(new JMenuItem("Exit") {
-                            {
-                                this.setBackground(new Color(16, 7, 232));
-                                this.setForeground(Color.WHITE);
-                                addActionListener(e -> frame.dispose());
-                            }
-                        });
-                    }
-                });
+	Giocatore(JFrame parent) {
+		combo = new boolean[5];
+		numero = 0;
+		caselle = new JButton[RIGHE * COLONNE];
+		frame = new JFrame("Tombola");
+		labelNumero = new JLabel("-Numero-");
+		panel1 = new JPanel();
+		panel2 = new JPanel();
+		griglia = new GridLayout(3, 9, 3, 3);
+		statusPanel = new JPanel();
 
-                add(new JMenu("Server") {
-                    {
-                        this.setBackground(new Color(16, 7, 232));
-                        this.setForeground(Color.WHITE);
-                        add(new JMenuItem("Connect To Server") {
-                            {
-                                this.setBackground(new Color(16, 7, 232));
-                                this.setForeground(Color.WHITE);
-                                addActionListener(e -> ConnectToServer());
-                            }
-                        });
-                        add(new JMenuItem("Disconect") {
-                            {
-                                this.setBackground(new Color(16, 7, 232));
-                                this.setForeground(Color.WHITE);
-                                addActionListener(e -> Disconnect());
-                            }
-                        });
-                    }
-                });
-            }
-        });
-        
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
-    // methods (GUI)
-    private void GeneraTabella() {
-        for(int i = 0; i < tabella.RIGHE; i++) 
-                for(int j = 0; j < tabella.COLONNE; j++) {
-                    caselle[j + tabella.RIGHE * i] = new JButton();
-                    if (tabella.getTabella(i + tabella.RIGHE * j) == -1) {
-                        panel2.add(caselle[j + tabella.RIGHE * i]); 
-                        caselle[j + tabella.RIGHE * i].setBackground(new Color(16, 7, 232));
-                        caselle[j + tabella.RIGHE * i].setFocusable(false);
-                    }
-                    else {
-                        caselle[j + tabella.RIGHE * i].setText(String.valueOf(tabella.getTabella(i + tabella.RIGHE * j)));
-                        panel2.add(caselle[j + tabella.RIGHE * i]);
-                        caselle[j + tabella.RIGHE * i].setFont(new Font("Roboto", Font.BOLD, 36));
-                        caselle[j + tabella.RIGHE * i].setBackground(new Color(16, 7, 232));
-                        caselle[j + tabella.RIGHE * i].setForeground(Color.WHITE);
-                        caselle[j + tabella.RIGHE * i].setFocusable(false);
-                    }
-            }
-    }
-
-    // methods (Networking)
-    private void ConnectToServer() {
-        try {
-            SocketAddress socketAddress = new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 60001);
-            socket.connect(socketAddress);
-            new Thread(() -> {ReadNumber();}).start();
-            System.out.println(socket.toString());
-        } catch (IOException e) {
-            System.err.println(e);
-        }
-    }
 
 		frame.setSize(600, 350);
 		frame.setLayout(new GridLayout(2,1));
