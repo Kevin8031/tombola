52c1cef8f38c2511a2368f52bb737fe1e3d16e70
Kevin8031 <concilio.kevin@gmail.com>
2022-01-03 13:05:54 +0100

    removed useless comment

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
+import gui.MainMenu;
+
 public class Main {
 	public static void main(String[] args) {
 		new MainMenu();
-		// Tabella t = new Tabella();
-		// t.generaTabella();
-
-		// String s = new String(t.toString());
-		// System.out.println(t.toString());
 	}
 }
