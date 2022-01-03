00e452bb1c9dce7fea1f676fe53d9acb914a2b80
Kevin8031 <concilio.kevin@gmail.com>
2022-01-03 23:34:03 +0100

    Save network constant here

---
+++ src/net/Common.java
@@ -0,0 +1,8 @@
+package net;
+
+public class Common {
+	
+	final protected int MULTICAST_PORT = 8888;
+	final protected String MULTICAST_INET = "237.5.6.7";
+	final protected int SERVER_PORT = 4321;
+}
